8264283ed53b49e126448559dece3bda2ee146e5
Hp-Gelmer_Marquez-W11 <gelmer023@gmail.com>
2022-10-12 16:53:25 -0500

    Se agrego la tarjeta de producto con manipulacion del DOM, sin usar mucho html y mas js con sus respetivo css

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const menuHamIcon= document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const cartIcon = document.querySelector('.navbar-shopping-cart');
 const assideOrdesMenu = document.querySelector('.product-detail');
+const cardsContainer= document.querySelector('.cards-container');
 
 // eventos
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -16,7 +17,7 @@ cartIcon.addEventListener('click', toggleAssiMenu);
 // toggleDesktopMenu muestra/oculta el submenu .desktopMenu con el email de cuenta
 function toggleDesktopMenu(event){
     desktopMenu.classList.toggle('inactive');
-    // desactivar el recargo del el link de .navbar-email 
+    // desactivar el recargo del el link de .navbar-email
     event.preventDefault();
 }
 // toggleMobileMenu muestra/oculta el menu con el icono de menu
@@ -40,3 +41,77 @@ function toggleAssiMenu(){
     }
     assideOrdesMenu.classList.toggle('inactive');
 }
+
+
+
+
+const productList = [];
+
+productList.push({
+    name: "Bicicleta",
+    buy: 120,
+    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+    name: "Camara",
+    buy: 230,
+    image: "https://images.pexels.com/photos/9910849/pexels-photo-9910849.jpeg?cs=srgb&dl=pexels-cottonbro-9910849.jpg&fm=jpg",
+});
+productList.push({
+    name: "Computador",
+    buy: 690,
+    image: "https://images.pexels.com/photos/7745560/pexels-photo-7745560.jpeg?cs=srgb&dl=pexels-screen-post-7745560.jpg&fm=jpg",
+});
+
+//  <div class="product-card">
+//         <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+//         <div class="product-info">
+//           <div>
+//             <p>$120,00</p>
+//             <p>Bike</p>
+//           </div>
+//           <figure>
+//             <img src="./icons/bt_add_to_cart.svg" alt="">
+//           </figure>
+//         </div>
+//       </div>
+for (product of productList){
+    // Aquie se crea la maquetacion del los productos
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.innerText='$' + product.buy;
+
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+    // Aquie se inyecta la maquetacion en sus respetivo orden
+    // productInfoDiv.appendChild(productPrice);
+    // productInfoDiv.appendChild(productName);
+    productInfoDiv.append(productPrice, productName);
+    productInfoFigure.appendChild(productImgCart);
+
+    // productInfo.appendChild(productInfoDiv);
+    // productInfo.appendChild(productInfoFigure);
+    productInfo.append(productInfoDiv, productInfoFigure);
+
+    // productCard.appendChild(productImg);
+    // productCard.appendChild(productInfo);
+
+    productCard.append(productImg, productInfo);
+
+    cardsContainer.appendChild(productCard);
+
+};
